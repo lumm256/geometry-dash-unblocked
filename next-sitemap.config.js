49c2c2d1a0fe53ab822a5893b98e674cebea2de7
pzh <1047719600@qq.com>
2024-10-10 10:03:56 +0800

    更换 logo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   siteUrl:
-    process.env.NEXT_PUBLIC_SITE_URL || "https://landingpage.weijunext.com",
+    process.env.NEXT_PUBLIC_SITE_URL,
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
