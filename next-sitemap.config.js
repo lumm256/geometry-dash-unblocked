40a4483d9aabd2feadf410c3e89a80060d576885
pzh <1047719600@qq.com>
2024-10-28 10:13:44 +0800

    更改网站地图

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,143 +6,143 @@ module.exports = {
   sitemapSize: 7000,
   alternateRefs: [
     {
-      href: 'https://geometry-dash-unblocked.com/ar',
+      href: 'https://geometry-dash-unblocked.com/ar/',
       hreflang: 'ar',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ar/geometry-dash',
+      href: 'https://geometry-dash-unblocked.com/ar/geometry-dash/',
       hreflang: 'ar',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ar/geometry-dash-cadenza',
+      href: 'https://geometry-dash-unblocked.com/ar/geometry-dash-cadenza/',
       hreflang: 'ar',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ar/geometry-dash-meltdown',
+      href: 'https://geometry-dash-unblocked.com/ar/geometry-dash-meltdown/',
       hreflang: 'ar',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ar/geometry-dash-subzero',
+      href: 'https://geometry-dash-unblocked.com/ar/geometry-dash-subzero/',
       hreflang: 'ar',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ar/geometry-dash-subzero-hacked',
+      href: 'https://geometry-dash-unblocked.com/ar/geometry-dash-subzero-hacked/',
       hreflang: 'ar',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ar/geometry-dash-world-toxic-factory',
+      href: 'https://geometry-dash-unblocked.com/ar/geometry-dash-world-toxic-factory/',
       hreflang: 'ar',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/es',
+      href: 'https://geometry-dash-unblocked.com/es/',
       hreflang: 'es',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/es/geometry-dash',
+      href: 'https://geometry-dash-unblocked.com/es/geometry-dash/',
       hreflang: 'es',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/es/geometry-dash-cadenza',
+      href: 'https://geometry-dash-unblocked.com/es/geometry-dash-cadenza/',
       hreflang: 'es',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/es/geometry-dash-meltdown',
+      href: 'https://geometry-dash-unblocked.com/es/geometry-dash-meltdown/',
       hreflang: 'es',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/es/geometry-dash-subzero',
+      href: 'https://geometry-dash-unblocked.com/es/geometry-dash-subzero/',
       hreflang: 'es',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/es/geometry-dash-subzero-hacked',
+      href: 'https://geometry-dash-unblocked.com/es/geometry-dash-subzero-hacked/',
       hreflang: 'es',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/es/geometry-dash-world-toxic-factory',
+      href: 'https://geometry-dash-unblocked.com/es/geometry-dash-world-toxic-factory/',
       hreflang: 'es',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ja',
+      href: 'https://geometry-dash-unblocked.com/ja/',
       hreflang: 'ja',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ja/geometry-dash',
+      href: 'https://geometry-dash-unblocked.com/ja/geometry-dash/',
       hreflang: 'ja',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ja/geometry-dash-cadenza',
+      href: 'https://geometry-dash-unblocked.com/ja/geometry-dash-cadenza/',
       hreflang: 'ja',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ja/geometry-dash-meltdown',
+      href: 'https://geometry-dash-unblocked.com/ja/geometry-dash-meltdown/',
       hreflang: 'ja',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ja/geometry-dash-subzero',
+      href: 'https://geometry-dash-unblocked.com/ja/geometry-dash-subzero/',
       hreflang: 'ja',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ja/geometry-dash-subzero-hacked',
+      href: 'https://geometry-dash-unblocked.com/ja/geometry-dash-subzero-hacked/',
       hreflang: 'ja',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ja/geometry-dash-world-toxic-factory',
+      href: 'https://geometry-dash-unblocked.com/ja/geometry-dash-world-toxic-factory/',
       hreflang: 'ja',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ru',
+      href: 'https://geometry-dash-unblocked.com/ru/',
       hreflang: 'ru',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ru/geometry-dash',
+      href: 'https://geometry-dash-unblocked.com/ru/geometry-dash/',
       hreflang: 'ru',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ru/geometry-dash-cadenza',
+      href: 'https://geometry-dash-unblocked.com/ru/geometry-dash-cadenza/',
       hreflang: 'ru',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ru/geometry-dash-meltdown',
+      href: 'https://geometry-dash-unblocked.com/ru/geometry-dash-meltdown/',
       hreflang: 'ru',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ru/geometry-dash-subzero',
+      href: 'https://geometry-dash-unblocked.com/ru/geometry-dash-subzero/',
       hreflang: 'ru',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ru/geometry-dash-subzero-hacked',
+      href: 'https://geometry-dash-unblocked.com/ru/geometry-dash-subzero-hacked/',
       hreflang: 'ru',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/ru/geometry-dash-world-toxic-factory',
+      href: 'https://geometry-dash-unblocked.com/ru/geometry-dash-world-toxic-factory/',
       hreflang: 'ru',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/zh',
+      href: 'https://geometry-dash-unblocked.com/zh/',
       hreflang: 'zh',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/zh/geometry-dash',
+      href: 'https://geometry-dash-unblocked.com/zh/geometry-dash/',
       hreflang: 'zh',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/zh/geometry-dash-cadenza',
+      href: 'https://geometry-dash-unblocked.com/zh/geometry-dash-cadenza/',
       hreflang: 'zh',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/zh/geometry-dash-meltdown',
+      href: 'https://geometry-dash-unblocked.com/zh/geometry-dash-meltdown/',
       hreflang: 'zh',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/zh/geometry-dash-subzero',
+      href: 'https://geometry-dash-unblocked.com/zh/geometry-dash-subzero/',
       hreflang: 'zh',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/zh/geometry-dash-subzero-hacked',
+      href: 'https://geometry-dash-unblocked.com/zh/geometry-dash-subzero-hacked/',
       hreflang: 'zh',
     },
     {
-      href: 'https://geometry-dash-unblocked.com/zh/geometry-dash-world-toxic-factory',
+      href: 'https://geometry-dash-unblocked.com/zh/geometry-dash-world-toxic-factory/',
       hreflang: 'zh',
     }
   ],
