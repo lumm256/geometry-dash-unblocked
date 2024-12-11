1679eb5932312c6765570320b42e97204de43c50
pzh <1047719600@qq.com>
2024-12-11 15:02:32 +0800

    去掉 header 、showcase 的 playxia 链接

--- components/home/Showcase.tsx
+++ components/home/Showcase.tsx
@@ -2,7 +2,6 @@ import WebsiteCard from "@/components/WebsiteCard";
 import { showcases } from "@/config/showcases";
 import { createModernMetaScraper, ModernMetaScraper } from "@/lib/metaScraper";
 import { Spacer } from "@nextui-org/react";
-import { ExternalLink } from "lucide-react";
 import { RoughNotation } from "react-rough-notation";
 
 const scraper: ModernMetaScraper = createModernMetaScraper();
@@ -31,7 +30,7 @@ const Showcase = async ({
         </h2>
         {/* <p className="text-large text-default-500">{locale.description}</p>  */}
       </div>
-      <a
+      {/* <a
         href="https://www.playxia.com/"
         target="_blank"
         aria-label="PlayXia Games"
@@ -41,7 +40,7 @@ const Showcase = async ({
         className="tracking-wide transition-colors duration-200 font-normal flex items-center text-blue-500"
       >
         More Games <ExternalLink className="h-4 w-4 ml-1" />
-      </a>
+      </a> */}
       <Spacer y={8} />
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 justify-items-center">
         {sites.map((site) => (
